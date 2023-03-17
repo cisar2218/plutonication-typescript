3f442a18f553713b50a5647da931083dd26008ff
cisar2218 <cisar2218@gmail.com>
2023-03-17 23:29:09 +0100

    AccessCredentials, MessageCode objects

---
+++ src/MessageCode.ts
@@ -0,0 +1,9 @@
+export enum MessageCode {
+    PublicKey = 0,
+    Success = 1,
+    Refused = 2,
+    Method = 3,
+    Auth = 4,
+    FilledOut = 5,
+    Extrinsic = 6,
+}
--- test/AccessCredentials.test.ts
+++ test/AccessCredentials.test.ts
@@ -1,7 +1,32 @@
 import { AccessCredentials } from "../src/AccessCredentials";
 
 describe("AccessCredentials", () => {
-    const ac = new AccessCredentials("127.0.0.1", 8080);
+    test("Constructors", () => {
+        const sampleAddress = "127.0.0.1"
+        const samplePort = 8080
+        const sampleKey = "sample password"
+        const sampleName = "dApp name"
+        const sampleIcon = "dApp icon";
+
+        let ac1 = new AccessCredentials(sampleAddress, samplePort);
+        let ac2 = new AccessCredentials(sampleAddress, samplePort, sampleKey);
+        let ac3 = new AccessCredentials(sampleAddress, samplePort, sampleKey, sampleName, sampleIcon);
+
+        expect(ac1.address).toBe(sampleAddress);
+        expect(ac1.port).toBe(samplePort);
+        expect(ac1.key).not.toBeUndefined();
+
+        expect(ac2.address).toBe(sampleAddress);
+        expect(ac2.port).toBe(samplePort);
+        expect(ac2.key).toBe(sampleKey);
+
+        expect(ac3.address).toBe(sampleAddress);
+        expect(ac3.port).toBe(samplePort);
+        expect(ac3.key).toBe(sampleKey);
+        expect(ac3.name).toBe(sampleName);
+        expect(ac3.icon).toBe(sampleIcon);
+    });
+
 
     test("Defined", () => {
         expect(AccessCredentials).not.toBeUndefined();
@@ -25,6 +50,8 @@ describe("AccessCredentials", () => {
     });
 
     describe("Key", () => {
+        let ac = new AccessCredentials("127.0.0.1", 8080);
+
         test("Key is defined (automatically)", () => {
             expect(ac.key).not.toBeUndefined();
         });
@@ -38,5 +65,32 @@ describe("AccessCredentials", () => {
             const defaultLen = 30;
             expect(ac.key.length).toBe(defaultLen);
         });
+    });
+
+    test("Convertion to Uri", () => {
+        const sampleAddress = "127.0.0.1"
+        const samplePort = 8080
+        const sampleKey = "sample password"
+        const sampleName = "dApp name"
+        const sampleIcon = "dApp icon";
+
+        let ac = new AccessCredentials(sampleAddress, samplePort, sampleKey, sampleName, sampleIcon);
+        expect(ac.ToUri().toString()).toBe(
+            "plutonication://?url=127.0.0.1%3A8080&key=sample+password&name=dApp+name&icon=dApp+icon"
+        );
+    });
+
+    test("Creation from Uri", () => {
+        const sampleAddress = "127.0.0.1"
+        const samplePort = 8080
+        const sampleKey = "sample password"
+        const sampleName = "dApp name"
+        const sampleIcon = "dApp icon";
+        
+        const url = new URL("plutonication://?url=127.0.0.1%3A8080&key=sample+password&name=dApp+name&icon=dApp+icon");
+        const fromUri = AccessCredentials.fromURL(url);
+        let ac = new AccessCredentials(sampleAddress, samplePort, sampleKey, sampleName, sampleIcon);
+
+        expect(ac).toStrictEqual(fromUri);
     });
 });
